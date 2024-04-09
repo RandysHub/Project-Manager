938dc0588c6cdbc134b4ea965f8dbf6cc9d8df6d
RandysHub <106208354+RandysHub@users.noreply.github.com>
2024-04-09 02:20:33 -0400

    styled a bunch

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,9 +1,10 @@
-const Input = ({ ref, label, labelClass, inputClass, textArea, ...props }) => {
+const Input = ({ ref, label, textArea, ...props }) => {
+  const classes = 'w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600'
   return (
-    <p>
-      <label className={labelClass} >{label}</label>
+    <p className="flex flex-col gap-1 my-4">
+      <label className="text-sm font-bold uppercase text-stone-500" >{label}</label>
 
-      {textArea ? <textarea ref={ref} className={inputClass}  {...props} /> : <input ref={ref} className={inputClass}  {...props} />}
+      {textArea ? <textarea ref={ref} className={classes}  {...props} /> : <input ref={ref} className={classes}  {...props} />}
     </p>
   )
 }
--- src/components/ProjectForm.jsx
+++ src/components/ProjectForm.jsx
@@ -2,9 +2,6 @@ import React, { useRef } from 'react'
 import Input from './Input'
 
 export default function ProjectForm({ projects, setProjects }) {
-  const inputStyling = "w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600"
-  const labelStyling = "text-sm font-bold uppercase text-stone-500"
-  const buttonStyling = "px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950"
 
 
   const name = useRef();
@@ -25,13 +22,17 @@ export default function ProjectForm({ projects, setProjects }) {
 
 
   return (
-    <form>
-      <Input ref={name} inputClass={inputStyling} labelClass={labelStyling} label={'Name'} type='text' />
-      <Input ref={description} inputClass={inputStyling} labelClass={labelStyling} label={'Description'} type='text' textArea={true} />
-      <Input ref={dueDate} inputClass={inputStyling} labelClass={labelStyling} label={'Due Date'} type='date' />
-      <button onClick={handleSave} className={buttonStyling}>Save</button>
-      <button className={buttonStyling}>Cancel</button>
-    </form>
+    <div className='w-[35rem] mt-16  '>
+      <menu className='flex  items-center justify-end gap-4 my-4'>
+        <li><button className='text-stone-800 hover:text-stone-950'>Cancel</button></li>
+        <li><button onClick={handleSave} className=' px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950'>Save</button></li>
+      </menu>
+      <form>
+        <Input ref={name} label={'Name'} type='text' />
+        <Input ref={description} label={'Description'} type='text' textArea={true} />
+        <Input ref={dueDate} label={'Due Date'} type='date' />
+      </form>
+    </div>
   )
 }
 
