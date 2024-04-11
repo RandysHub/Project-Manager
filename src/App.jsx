09162a9bf5a02aba8f8b9fad358ef45d7013bf8e
RandysHub <106208354+RandysHub@users.noreply.github.com>
2024-04-11 19:47:58 -0400

    Delete function added

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,21 @@ import Project from './components/Project';
 import NoProjectSelected from './components/NoProjectSelected';
 
 function App() {
-  const [projects, setProjects] = useState([{}]);
+  const [projects, setProjects] = useState([]);
   const [content, setContent] = useState('none');
   const [currentProject, setCurrentProject] = useState();
 
   let display;
 
+  function handleDelete() {
+    setProjects(projects.filter(project => project.id !== currentProject.id))
+  }
 
 
   if (content === 'creating') {
     display = <ProjectForm projects={projects} setProjects={setProjects} setContent={setContent} />
   } else if (content === 'show') {
-    display = <Project currentProject={currentProject} setCurrentProject={setCurrentProject} />
+    display = <Project currentProject={currentProject} setCurrentProject={setCurrentProject} handleDelete={handleDelete} />
   } else {
     display = <NoProjectSelected setContent={setContent} />
   }
--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,8 +1,8 @@
 import { useRef } from "react"
 import Input from "./Input";
-import Button from "./Button";
 
-const Project = ({ currentProject, setCurrentProject }) => {
+const Project = ({ currentProject, setCurrentProject, handleDelete }) => {
+
   const input = useRef();
 
   const formattedDate = new Date(currentProject.dueDate).toLocaleDateString('en-US', {
@@ -21,12 +21,13 @@ const Project = ({ currentProject, setCurrentProject }) => {
     console.log(currentProject);
 
   }
+
   return (
     <div className="w-[35rem] mt-16">
       <header className="pb-4 mb-4 border-b-2  border-stone-300">
         <div className="flex items-center justify-between">
           <h1 className="text-3xl font-bold text-stone-600 mb-2">{currentProject.name}</h1>
-          <button className="text-stone-500 hover:text-stone-950">Delete</button>
+          <button onClick={handleDelete} className="text-stone-500 hover:text-stone-950">Delete</button>
         </div>
         <p className="mb-4 text-stone-400">{formattedDate}</p>
         <p className="text-stone-600 whitespace-pre-wrap">{currentProject.description}</p>
